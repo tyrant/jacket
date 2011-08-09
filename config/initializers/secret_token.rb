1c31b4780d69d8248b82606e9e1ffe955e5762ec
Mikey <pineutrino@gmail.com>
2011-08-09 12:59:16 +1200

    First commit for Dust Jacket project\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jacket::Application.config.secret_token = '43190afa7abca580efe20b6c8559d5bf722ac186015b4e9f29a5a18645733fadc5e441559b8fa9586c3e72471067625fc00ad5ce425c90194611f46be19648a7'
